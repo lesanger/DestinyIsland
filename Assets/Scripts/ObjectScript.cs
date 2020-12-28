e4f72c3ae86671493c87e4c5909085576a8f7b49
Kovalev Nikita <lesanger@yandex.ru>
2020-12-28 23:56:35 +0300

    Модели активируются после успешного разговора

--- Assets/Scripts/ObjectScript.cs
+++ Assets/Scripts/ObjectScript.cs
@@ -9,7 +9,8 @@ public class ObjectScript : MonoBehaviour
     public Transform spawnPos;
     
     private GameObject player;
-    private GameObject camera;
+    private GameObject mainCamera;
+    public GameObject model;
 
     private float countToDeath = 3f;
     private float inWhatTimeDie;
@@ -18,7 +19,7 @@ public class ObjectScript : MonoBehaviour
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        camera = GameObject.FindWithTag("MainCamera");
+        mainCamera = GameObject.FindWithTag("MainCamera");
     }
 
     private void Update()
@@ -34,20 +35,19 @@ public class ObjectScript : MonoBehaviour
 
     public void TalkIsDone()
     {
-        // Удаляем NPC и даем отмашку менеджеру сцены на включение объекта по ID
         Debug.Log("Разговор окончен");
+        model.SetActive(true);
         Destroy(gameObject);
     }
 
     public void ButtonPressed()
     {
         player.transform.position = spawnPos.transform.position;
-        //Vector3 newRot = spawnPos.transform.rotation.eulerAngles;
         player.transform.eulerAngles = spawnPos.transform.rotation.eulerAngles;
 
         // Запускам триггер для анимации лифта HUD
 
-        if (camera.TryGetComponent<CameraConroller>(out CameraConroller cameraController))
+        if (mainCamera.TryGetComponent<CameraConroller>(out CameraConroller cameraController))
         {
             cameraController.ResetCamera();
         }
