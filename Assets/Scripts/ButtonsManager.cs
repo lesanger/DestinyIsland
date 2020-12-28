cbf4448fd943489dcb907aed274b5df93a5f5514
Kovalev Nikita <lesanger@yandex.ru>
2020-12-29 00:57:54 +0300

    Убрал спам сообщения в консоль

--- Assets/Scripts/ButtonsManager.cs
+++ Assets/Scripts/ButtonsManager.cs
@@ -6,12 +6,15 @@ public class ButtonsManager : MonoBehaviour
 {
     public GameObject theGirl;
     public int buttonsCounter = 0;
+    private bool isGirlExist = false;
 
     void Update()
     {
-        if (buttonsCounter >= 14)
+        if ((buttonsCounter >= 14) && (isGirlExist == false))
         {
             theGirl.SetActive(true);
+            isGirlExist = true;
+            
             Debug.Log("Девушка была создана!");
         }
     }
